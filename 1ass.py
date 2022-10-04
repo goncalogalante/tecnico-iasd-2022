920f48d533e2213862c6af27dba6727fde24da65
Gonçalo Galante <72885029+goncalogalante@users.noreply.github.com>
2022-10-04 23:01:08 +0100

    updating comments to improve readability

--- 1ass.py
+++ 1ass.py
@@ -1,41 +1,47 @@
 #import search
 import numpy as np
 
-class RTBProblem (): #class RTBProblem (search.problem):
+## Class RTBPRoblem (Search.problem)
+class RTBProblem (): 
     def __init__ (self): 
         self.map=[]
         self.N=0
         self.beg=(-1,-1)
         self.end=(-1,-1)
     def load (self,fh):
+        
+        ## Reads the comment lines and stores the size of the puzzle
         a=fh.readline()
-
         while a[0]=='#':
             a=fh.readline()
             pass
-        self.N=int(a[0])
-        #print(self.N)
-        for i in range (0,self.N): #tirar as linhas de texto para o mapa (matriz)
+        self.N=int(a[0:-1])
+     
+        ## For each line of text saves it to the map matrix and make some verifications 
+        for i in range (0,self.N):
             l=fh.readline()
-            #print(l)
             (self.map).append(l.split(' '))
-            if ((self.map)[i][-1])[-1] =='\n':  #tirar \n
+            
+            ## Removes the character \n of the line
+            if ((self.map)[i][-1])[-1] =='\n': 
                 (self.map)[i][-1]= ((self.map)[i][-1])[0:-1]
                 pass
-            if (self.beg == (-1,-1)): #início not found
+            
+            ## Checks if the initial state exists and stores its coordinates 
+            if (self.beg == (-1,-1)): 
                 j=0
                 while (j<self.N and ((self.map)[i][j])[0]!='i'):
-                    #print("i->",i,"j->",j,"((self.map)[i][j])[0]",((self.map)[i][j])[0],"self.beg =",self.beg)
                     j+=1
                     pass
                 if j<self.N:
                     self.beg=(i,j)
                     pass
                 pass
-            if (self.end == (-1,-1)): #fim not found
+            
+            ## Checks if the goal state exists and stores its coordinates
+            if (self.end == (-1,-1)):
                 j=0
                 while (j<self.N and ((self.map)[i][j])[0]!='g'):
-                    #print("i->",i,"j->",j,"((self.map)[i][j])[0]",((self.map)[i][j])[0],"self.end =",self.end)
                     j+=1
                     pass
                 if j<self.N:
@@ -45,87 +51,99 @@ class RTBProblem (): #class RTBProblem (search.problem):
             pass
         return self
     
-    def isSolution (self) :
+    def isSolution (self):
         xi=self.beg[0]
         yi=self.beg[1]
         answer=True
         
+        ## Defines the next move from the initial state coordinates
         nextm=(self.map[xi][yi].split('-'))[1]
-        #print(nextm)
         print(xi,yi,self.map[xi][yi])
-        answer=movevalid(nextm,xi,yi,self)
-#         while (movevalid(nextm,xi,yi,prob)!= (-1,-1) or movevalid(nextm,xi,yi,prob)!= (self.end[0],self.end[1])):
-#                 nextm=(self.map[xi][yi].split('-'))[1]
-#             pass
+        
+        ## Loop that evaluates the cells from the initial state to the goal state
+        ## and feedbacks it according to its coordinates
+        while (xi,yi)!=(self.end):
+            
+            ## Checks if the move is valid
+            (nextm,xi,yi,answer) = movevalid(nextm,xi,yi,self)
+            if(answer==True): 
+                
+                ## Returns True if the cell is the goal state
+                if(xi,yi)==self.end:
+                    if((self.map[xi][yi].split('-'))[1] == nextm):
+                        return True
+                    else:
+                        return False
+                
+                ## Evaluates if the cell is not the goal state or an empty one    
+                if((self.map[xi][yi].split('-'))[0] == nextm):
+                    nextm=(self.map[xi][yi].split('-'))[1]
+                    pass
+                elif((self.map[xi][yi].split('-'))[1] == nextm):
+                    nextm=(self.map[xi][yi].split('-'))[0]
+                    pass
+                
+                ## Returns False if it is none of the above
+                else:
+                    print("bad move",xi,yi,self.map[xi][yi])
+                    return False
+                pass
+            else:
+                return False
+            pass
+        
+        ## Returns True if the puzzle is already a solution
         return answer
 
+## Checks if a move its valid or invalid according to its coordinates and return it.
 def movevalid(move,xi,yi,prob):
     if(move=="left"):
-        if (yi-1)<0: #inválido
-            xf=-1
-            yf=-1
-            return False
+        if (yi-1)<0: # Invalid
+            xf=xi
+            yf=yi
+            return (move,xf,yf,False)
         else:
             xf=xi
             yf=yi-1
             move="right"
     elif(move=="right"):
-        if (yi+1)<prob.N: #válido
+        if (yi+1)<prob.N: # Valid
             xf=xi
             yf=yi+1
             move="left"
         else:
-            xf=-1
-            yf=-1
-            return False
+            xf=xi
+            yf=yi
+            return (move,xf,yf,False)
     elif(move=="down"):
-        if (xi+1)<prob.N: #válido
+        if (xi+1)<prob.N: # Valid
             xf=xi+1
             yf=yi
             move="top"
         else:
-            xf=-1
-            yf=-1
-            return False
+            xf=xi
+            yf=yi
+            return (move,xf,yf,False)
     elif(move=="top"):
-        if (xi-1)<0: #inválido
-            xf=-1
-            yf=-1
-            return False
+        if (xi-1)<0: # Invalid
+            xf=xi
+            yf=yi
+            return (move,xf,yf,False)
         else:
             xf=xi-1
             yf=yi
             move="down"
 
-    return checkcell(move,xf,yf,prob)
+    return (move,xf,yf,True)
 
-def checkcell(move,xf,yf,prob):
-    print(xf,yf,prob.map[xf][yf])
-    
-    if(xf,yf)==prob.end:
-        return True
-    
-    if((prob.map[xf][yf].split('-'))[0] == move):
-        return movevalid((prob.map[xf][yf].split('-'))[1],xf,yf,prob)
-    elif((prob.map[xf][yf].split('-'))[1] == move):
-        return movevalid((prob.map[xf][yf].split('-'))[0],xf,yf,prob)
-    else:
-        print("bad move")
-        return False
 
 def main():
-    fh=open("Fig8-T.txt")
+    fh=open("Arquivos/pub05.dat")
     prob=RTBProblem()
     prob=RTBProblem.load(prob,fh)
-#     print(prob.map)
-#     print(prob.N)
-#     print(prob.beg)
-#     print((prob.map)[prob.beg[0]][prob.beg[1]])
-#     print(prob.end)
-#     print((prob.map)[prob.end[0]][prob.end[1]])
-    
     result=RTBProblem.isSolution(prob)
+    print("deu certo? ->",result)
     
-
+# Executes the main function
 if __name__ == "__main__":
     main()
